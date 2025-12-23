94fbba6978483d0269fd3235cdbd547b985ec8ca
KangJunBeom <sorabol623@gmail.com>
2025-12-23 13:54:11 +0900

    feat(Servo): 큐 통신 방식 일부 교체

--- App/ServoTask/Src/ServoTask.cpp
+++ App/ServoTask/Src/ServoTask.cpp
@@ -8,35 +8,26 @@ void ServoMotorTaskHandler(void *argument){
     
     int angle = 0; //example angle to move
     int delay = 0.0;
-    TIM_HandleTypeDef htim;
+    extern TIM_HandleTypeDef htim2;
     extern osMessageQueueId_t ServoAngleQueueHandle;
     extern osMessageQueueId_t DistanceStartQueueHandle;
-    const osMessageQueueAttr_t DistanceStartQueue_attributes = {
-        .name = "DistanceStartQueue"
-    };
 
-    //get the angle data from ServoAngleQueue
-    if(osMessageQueueGet(ServoAngleQueueHandle, &angle, NULL, osWaitForever) == osOK) {
+    for(;;)
+    {
+        if(osMessageQueueGet(ServoAngleQueueHandle, &angle, NULL, osWaitForever) == osOK) {
 
         if(angle < 0) angle = 0;
         if(angle > 180) angle = 180;
 
         int pulse_val = 50 + (angle * 200 / 180);
-        __HAL_TIM_SET_COMPARE(&htim, TIM_CHANNEL_1, pulse_val);
+        __HAL_TIM_SET_COMPARE(&htim2, TIM_CHANNEL_1, pulse_val);
         
         //delay for servo to reach the position (approx 320ms for 180 degrees)
         delay = (angle*100 / 180) * 320;
-
-        //Create the queue to send signal to Distance Sensor Task
-        DistanceStartQueueHandle = osMessageQueueNew (8, sizeof(uint8_t), &DistanceStartQueue_attributes);
         
         //Send signal to Distance Sensor Task to start measurement(dummy data)
         uint8_t startSignal = 1;
         osMessageQueuePut(DistanceStartQueueHandle, &startSignal, 0, delay);
     }
-
-    for(;;)
-    {
-        osDelay(1);
     }
 }
