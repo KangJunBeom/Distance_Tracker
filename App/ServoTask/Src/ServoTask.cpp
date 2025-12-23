f69d9b4f9ec5051a07c1cb406bf55f56e2945b87
KangJunBeom <sorabol623@gmail.com>
2025-12-23 10:44:23 +0900

    feat(Servo): 큐 통신이 정상적으로 진행 시 작동하도록 방식 변경

--- App/ServoTask/Src/ServoTask.cpp
+++ App/ServoTask/Src/ServoTask.cpp
@@ -6,7 +6,7 @@
 extern "C"
 void ServoMotorTaskHandler(void *argument){
     
-    int angle = 100; //example angle to move
+    int angle = 0; //example angle to move
     int delay = 0.0;
     TIM_HandleTypeDef htim;
     extern osMessageQueueId_t ServoAngleQueueHandle;
@@ -16,23 +16,24 @@ void ServoMotorTaskHandler(void *argument){
     };
 
     //get the angle data from ServoAngleQueue
-    osMessageQueueGet(ServoAngleQueueHandle, &angle, NULL, osWaitForever);
+    if(osMessageQueueGet(ServoAngleQueueHandle, &angle, NULL, osWaitForever) == osOK) {
 
-    if(angle < 0) angle = 0;
-    if(angle > 180) angle = 180;
+        if(angle < 0) angle = 0;
+        if(angle > 180) angle = 180;
 
-    int pulse_val = 50 + (angle * 200 / 180);
-    __HAL_TIM_SET_COMPARE(&htim, TIM_CHANNEL_1, pulse_val);
-    
-    //delay for servo to reach the position (approx 320ms for 180 degrees)
-    delay = (angle*100 / 180) * 320;
+        int pulse_val = 50 + (angle * 200 / 180);
+        __HAL_TIM_SET_COMPARE(&htim, TIM_CHANNEL_1, pulse_val);
+        
+        //delay for servo to reach the position (approx 320ms for 180 degrees)
+        delay = (angle*100 / 180) * 320;
 
-    //Create the queue to send signal to Distance Sensor Task
-    DistanceStartQueueHandle = osMessageQueueNew (8, sizeof(uint8_t), &DistanceStartQueue_attributes);
-    
-    //Send signal to Distance Sensor Task to start measurement(dummy data)
-    uint8_t startSignal = 1;
-    osMessageQueuePut(DistanceStartQueueHandle, &startSignal, 0, delay);
+        //Create the queue to send signal to Distance Sensor Task
+        DistanceStartQueueHandle = osMessageQueueNew (8, sizeof(uint8_t), &DistanceStartQueue_attributes);
+        
+        //Send signal to Distance Sensor Task to start measurement(dummy data)
+        uint8_t startSignal = 1;
+        osMessageQueuePut(DistanceStartQueueHandle, &startSignal, 0, delay);
+    }
 
     for(;;)
     {
