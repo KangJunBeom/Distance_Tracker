b2853ab870f15d6123afaaad5ea938a24aec4693
kalsdn <kmwjjang1228@ajou.ac.kr>
2025-12-24 12:30:50 +0900

    fix(ultrasound): osMessageQueueGet 수정

--- App/DistanceTask/Src/DistanceTask.cpp
+++ App/DistanceTask/Src/DistanceTask.cpp
@@ -7,8 +7,10 @@
 extern osMessageQueueId_t DistanceStartQueueHandle;
 extern osMessageQueueId_t UartTxQueueHandle;
 
+//select falling or rising
 uint16_t RISING_FLAG = 1;
-uint8_t DISTANCE = 0;
+
+double DISTANCE = 0;
 uint32_t start_time = 0;
 uint32_t end_time = 0;
 
@@ -21,6 +23,7 @@ void DistanceSensorTaskHandler(void *argument){
     {
         if(osMessageQueueGet(DistanceStartQueueHandle,&signal,
           0, osWaitForever) == osOK) {
+            //activate ultrasound sensor
             HAL_GPIO_WritePin(GPIOA, GPIO_PIN_12, GPIO_PIN_SET);
             osDelay(1);
             HAL_GPIO_WritePin(GPIOA, GPIO_PIN_12, GPIO_PIN_RESET);
@@ -28,31 +31,32 @@ void DistanceSensorTaskHandler(void *argument){
     }
 }
 
-
+//start measuring distance
 void HAL_TIM_IC_CaptureCallback(TIM_HandleTypeDef *htim)
 {
   if (htim->Channel == HAL_TIM_ACTIVE_CHANNEL_1 && RISING_FLAG)
   {
-    start_time = HAL_TIM_ReadCapturedValue(htim, TIM_CHANNEL_1); // 시점 저장
-    // 이제 하강 엣지를 잡도록 설정 변경
+    start_time = HAL_TIM_ReadCapturedValue(htim, TIM_CHANNEL_1);
+
+    //change mode to falling edge
     __HAL_TIM_SET_CAPTUREPOLARITY(htim, TIM_CHANNEL_1, TIM_INPUTCHANNELPOLARITY_FALLING);
     RISING_FLAG = 0;
   }
   else if (htim->Channel == HAL_TIM_ACTIVE_CHANNEL_1 && !(RISING_FLAG))
   {
-    end_time = HAL_TIM_ReadCapturedValue(htim, TIM_CHANNEL_1); // 시점 저장
-    // 다시 상승 엣지를 잡도록 설정 변경
+    end_time = HAL_TIM_ReadCapturedValue(htim, TIM_CHANNEL_1);
     
+    //change mode to rising edge
+    __HAL_TIM_SET_CAPTUREPOLARITY(htim, TIM_CHANNEL_1, TIM_INPUTCHANNELPOLARITY_RISING);
+    RISING_FLAG = 1;
+
+    //calculate distance
     if (end_time > start_time) {
       DISTANCE = (end_time - start_time) / 58;
     } else {
       DISTANCE = ((65535 - end_time) + start_time) / 58;
     }
-
-    osMessageQueuePut(UartTxQueueHandle, &DISTANCE,
-      0, osWaitForever);
-
-    __HAL_TIM_SET_CAPTUREPOLARITY(htim, TIM_CHANNEL_1, TIM_INPUTCHANNELPOLARITY_RISING);
-    RISING_FLAG = 1;
+    
+    osMessageQueuePut(UartTxQueueHandle, &DISTANCE, 0, 0);
   }
 }
