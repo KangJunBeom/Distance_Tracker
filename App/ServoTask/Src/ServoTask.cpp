a0f80ff4f0159dbaef2eb59798f01ec80ec3ba12
KangJunBeom <sorabol623@gmail.com>
2025-12-24 14:08:46 +0900

    feat(servo): 이슈 해결 및 최종 실험 진행

--- App/ServoTask/Src/ServoTask.cpp
+++ App/ServoTask/Src/ServoTask.cpp
@@ -2,32 +2,37 @@
 #include "stm32f1xx_hal.h"
 #include "FreeRTOS.h"
 #include "cmsis_os.h"
+#include "stm32f1xx_hal_uart.h"
 #include "tim.h"
+#include <cstdint>
+#include <stdlib.h>
+
+extern osMessageQueueId_t ServoAngleQueueHandle;
+extern osMessageQueueId_t DistanceStartQueueHandle;
 
 extern "C"
 void ServoMotorTaskHandler(void *argument){
     
-    int angle = 0; //example angle to move
-    int delay = 0.0;
-    extern osMessageQueueId_t ServoAngleQueueHandle;
-    extern osMessageQueueId_t DistanceStartQueueHandle;
+    HAL_TIM_PWM_Start(&htim2, TIM_CHANNEL_1);
+    int angle = 0;
 
     for(;;)
-    {
-        if(osMessageQueueGet(ServoAngleQueueHandle, &angle, NULL, osWaitForever) == osOK) {
-
+    {   
+        
+        if(osMessageQueueGet(ServoAngleQueueHandle, &angle, NULL, 0) == osOK) {
+            
             if(angle < 0) angle = 0;
             if(angle > 180) angle = 180;
 
             int pulse_val = 50 + (angle * 200 / 180);
             __HAL_TIM_SET_COMPARE(&htim2, TIM_CHANNEL_1, pulse_val);
-        
-            //delay for servo to reach the position (approx 320ms for 180 degrees)
-            delay = (angle*100 / 180) * 320;
-        
-            //Send signal to Distance Sensor Task to start measurement(dummy data)
+            
             uint8_t startSignal = 1;
-            osMessageQueuePut(DistanceStartQueueHandle, &startSignal, 0, delay);
+            uint32_t t = (angle * 320 / 180);
+            osDelay(t); 
+            osMessageQueuePut(DistanceStartQueueHandle, &startSignal, 0, 0);
+
         }
     }
+    
 }
