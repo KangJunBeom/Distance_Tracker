3f6857f6cf5b6a21c647293bee794a7904f2c998
kalsdn <kmwjjang1228@ajou.ac.kr>
2025-12-24 14:42:03 +0900

    fix(ultrasound): 구조  개선 및 callback 함수 수정

--- App/DistanceTask/Src/DistanceTask.cpp
+++ App/DistanceTask/Src/DistanceTask.cpp
@@ -8,9 +8,9 @@ extern osMessageQueueId_t DistanceStartQueueHandle;
 extern osMessageQueueId_t UartTxQueueHandle;
 
 //select falling or rising
-uint16_t RISING_FLAG = 1;
+uint16_t Rising_flag = 1;
 
-double DISTANCE = 0;
+double Distance = 0;
 uint32_t start_time = 0;
 uint32_t end_time = 0;
 
@@ -34,29 +34,31 @@ void DistanceSensorTaskHandler(void *argument){
 //start measuring distance
 void HAL_TIM_IC_CaptureCallback(TIM_HandleTypeDef *htim)
 {
-  if (htim->Channel == HAL_TIM_ACTIVE_CHANNEL_1 && RISING_FLAG)
-  {
-    start_time = HAL_TIM_ReadCapturedValue(htim, TIM_CHANNEL_1);
+  if(htim->Instance == TIM3 && htim->Channel == HAL_TIM_ACTIVE_CHANNEL_1) {
+    if (Rising_flag)
+    {
+      start_time = HAL_TIM_ReadCapturedValue(htim, TIM_CHANNEL_1);
 
-    //change mode to falling edge
-    __HAL_TIM_SET_CAPTUREPOLARITY(htim, TIM_CHANNEL_1, TIM_INPUTCHANNELPOLARITY_FALLING);
-    RISING_FLAG = 0;
-  }
-  else if (htim->Channel == HAL_TIM_ACTIVE_CHANNEL_1 && !(RISING_FLAG))
-  {
-    end_time = HAL_TIM_ReadCapturedValue(htim, TIM_CHANNEL_1);
-    
-    //change mode to rising edge
-    __HAL_TIM_SET_CAPTUREPOLARITY(htim, TIM_CHANNEL_1, TIM_INPUTCHANNELPOLARITY_RISING);
-    RISING_FLAG = 1;
-
-    //calculate distance
-    if (end_time > start_time) {
-      DISTANCE = (end_time - start_time) / 58;
-    } else {
-      DISTANCE = ((65535 - end_time) + start_time) / 58;
+      //change mode to falling edge
+      __HAL_TIM_SET_CAPTUREPOLARITY(htim, TIM_CHANNEL_1, TIM_INPUTCHANNELPOLARITY_FALLING);
+      Rising_flag = 0;
     }
-    
-    osMessageQueuePut(UartTxQueueHandle, &DISTANCE, 0, 0);
+    else
+    {
+      end_time = HAL_TIM_ReadCapturedValue(htim, TIM_CHANNEL_1);
+      
+      //change mode to rising edge
+      __HAL_TIM_SET_CAPTUREPOLARITY(htim, TIM_CHANNEL_1, TIM_INPUTCHANNELPOLARITY_RISING);
+      Rising_flag = 1;
+
+      //calculate distance
+      if (end_time > start_time) {
+        Distance = (double)(end_time - start_time) / 58;
+      } else {
+        Distance = (double)((65535 - end_time) + start_time) / 58;
+      }
+      
+      osMessageQueuePut(UartTxQueueHandle, &Distance, 0, 0);
+    }
   }
 }
