7a3e3d2ac15022fecc4d7e582c4535f863e204bb
KangJunBeom <sorabol623@gmail.com>
2026-01-05 15:00:41 +0900

    feat(SG90): 기존 태스크의 클래스화

--- Drivers/SG90/Inc/SG90.h
+++ Drivers/SG90/Inc/SG90.h
@@ -2,7 +2,16 @@
 #define __SR04_H__
 
 class SG90{
+public:
+    SG90(TIM_HandleTypeDef* htim, uint32_t channel);
 
+    // 플로우차트의 'Servo 모터 각도를 Angle로 설정' 및 '이동할 때까지 기다리기'를 위한 인터페이스
+    uint32_t setAngle(int angle);
+
+private:
+    TIM_HandleTypeDef* _htim;
+    uint32_t _channel;
+    int _currentAngle;
 };
 
 #endif // __SR04_H__
